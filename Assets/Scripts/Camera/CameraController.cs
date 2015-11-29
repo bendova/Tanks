64f316440d755e441ae275507c3ce28a6629a40c
Marius <mariusvalentin.stan@gmail.com>
2015-11-29 15:08:12 +0200

    Initial commit.

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform m_Target;
+
+    private Vector3 m_Offset;
+
+	void Start()
+	{
+	    m_Offset = transform.position - m_Target.position;
+	}
+	
+	void LateUpdate()
+	{
+	    transform.position = m_Target.position + m_Offset;
+	}
+}
---
+++ Assets/Scripts/Player/TankController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankController : MonoBehaviour
+{
+    public float SpeedFactor = 10.0f;
+
+    private static Vector3 s_UpVector = new Vector3(0.0f, 1.0f, 0.0f);
+
+	void Start()
+    {
+	
+	}
+	
+	void Update()
+	{
+	    UpdateMovement();
+	}
+
+    private void UpdateMovement()
+    {
+        float hInput = Input.GetAxis("Horizontal1");
+        float vInput = Input.GetAxis("Vertical1");
+
+        Vector3 forwardDirection = Vector3.ProjectOnPlane(transform.forward, s_UpVector).normalized;
+        transform.position = transform.position + forwardDirection * vInput * SpeedFactor;
+        transform.Rotate(s_UpVector, hInput, Space.World);
+    }
+}
