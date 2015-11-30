b8f846c06f9a093b8753c87192ee42fe71e0e99b
Marius <mariusvalentin.stan@gmail.com>
2015-11-30 15:30:04 +0200

    Added the ability to fire shells from the tank.

---
+++ Assets/Scripts/Player/ShellController.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShellController : MonoBehaviour
+{
+    public float m_LaunchSpeed = 10.0f;
+    public GameObject m_Explosion;
+
+    private Rigidbody m_RigidBody;
+    private Vector3 m_ForwardDirection;
+    private float m_LaunchAngle;
+    private float m_SinLaunchAngle;
+    private float m_CosLaunchAngle;
+    private float m_TimeSinceLaunch = 0.0f;
+    private Vector3 m_LaunchPosition;
+    private Quaternion m_InitialRotation;
+
+    void Start()
+	{
+	    m_RigidBody = GetComponent<Rigidbody>();
+        m_RigidBody.useGravity = false;
+
+        m_LaunchPosition = transform.position;
+        m_LaunchAngle = (360f - transform.rotation.eulerAngles.x) * Mathf.Deg2Rad;
+        m_InitialRotation = transform.rotation;
+        m_InitialRotation.x = 0f;
+        m_InitialRotation.z = 0f;
+        m_ForwardDirection = Vector3.ProjectOnPlane(transform.forward, Vector3.up).normalized;
+        m_CosLaunchAngle = Mathf.Cos(m_LaunchAngle);
+        m_SinLaunchAngle = Mathf.Sin(m_LaunchAngle);
+	}
+
+    void Update()
+    {
+        m_TimeSinceLaunch += Time.fixedDeltaTime;
+        Vector2 parabolicPos = GetProjectilePosition(m_TimeSinceLaunch);
+        UpdatePosition(parabolicPos);
+        UpdateOrientation(parabolicPos);
+    }
+
+    private void UpdatePosition(Vector2 parabolicPos)
+    {
+        Vector3 nextPos = m_ForwardDirection * parabolicPos.x + Vector3.up * parabolicPos.y;
+        transform.position = m_LaunchPosition + nextPos;
+    }
+
+    private void UpdateOrientation(Vector2 parabolicPos)
+    {
+        Vector2 nextParabolicPos = GetProjectilePosition(m_TimeSinceLaunch + Time.fixedDeltaTime);
+        Vector3 nextOrientation = m_ForwardDirection * (nextParabolicPos.x - parabolicPos.x) +
+            Vector3.up * (nextParabolicPos.y - parabolicPos.y);
+        float angle = Vector3.Angle(m_ForwardDirection, nextOrientation) * Mathf.Deg2Rad;
+        if (nextOrientation.y < 0)
+        {
+            angle = -angle;
+        }
+        Quaternion newRotation = Quaternion.FromToRotation(m_ForwardDirection, nextOrientation);
+        transform.rotation = newRotation * m_InitialRotation;
+    }
+
+    private Vector2 GetProjectilePosition(float timeSinceLaunch)
+    {
+        float x = m_LaunchSpeed * m_CosLaunchAngle * timeSinceLaunch;
+        float y = m_LaunchSpeed * m_SinLaunchAngle * timeSinceLaunch - (Physics.gravity.magnitude * m_TimeSinceLaunch * timeSinceLaunch) / 2;
+        return new Vector2(x, y);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        SpawnExplosion();
+        GameObject.Destroy(gameObject);
+    }
+
+    private void SpawnExplosion()
+    {
+        GameObject explosion = GameObject.Instantiate(m_Explosion, transform.position, Quaternion.identity) as GameObject;
+        float destroyTime = 1f;
+        if (explosion)
+        {
+            ParticleSystem particles = explosion.GetComponent<ParticleSystem>();
+            if (particles)
+            {
+                destroyTime = particles.duration;
+            }
+        }
+        Destroy(explosion, destroyTime);
+    }
+}
--- Assets/Scripts/Player/TankController.cs
+++ Assets/Scripts/Player/TankController.cs
@@ -3,27 +3,46 @@ using System.Collections;
 
 public class TankController : MonoBehaviour
 {
-    public float SpeedFactor = 10.0f;
-
-    private static Vector3 s_UpVector = new Vector3(0.0f, 1.0f, 0.0f);
+    public float m_SpeedFactor = 0.2f;
+    public Transform m_ShellSpawner;
+    public GameObject m_Projectile;
+    public float m_ProjectileAcc = 1.0f;
 
 	void Start()
-    {
-	
+	{
 	}
 	
 	void Update()
 	{
 	    UpdateMovement();
+	    UpdateFiring();
 	}
-
+    
     private void UpdateMovement()
     {
         float hInput = Input.GetAxis("Horizontal1");
         float vInput = Input.GetAxis("Vertical1");
+        
+        Vector3 forwardDirection = Vector3.ProjectOnPlane(transform.forward, Vector3.up).normalized;
+        transform.position = transform.position + forwardDirection * vInput * m_SpeedFactor;
+        transform.Rotate(Vector3.up, hInput, Space.World);
+    }
 
-        Vector3 forwardDirection = Vector3.ProjectOnPlane(transform.forward, s_UpVector).normalized;
-        transform.position = transform.position + forwardDirection * vInput * SpeedFactor;
-        transform.Rotate(s_UpVector, hInput, Space.World);
+    private void UpdateFiring()
+    {
+        bool fire = Input.GetButtonUp("Fire1");
+        if (fire)
+        {
+            FireProjectile();
+        }
+    }
+
+    private void FireProjectile()
+    {
+        GameObject projectile = GameObject.Instantiate(m_Projectile, m_ShellSpawner.position, m_ShellSpawner.rotation) as GameObject;
+        if (projectile)
+        {
+            projectile.GetComponent<ShellController>().m_LaunchSpeed = m_ProjectileAcc;
+        }
     }
 }
